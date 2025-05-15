9015d8b813e3586a9489202355c002caec6300ab
kubivan <kubivan@users.noreply.github.com>
2025-05-16 01:22:31 +0300

    chore: remove redundant imports

--- src/SC2/Army/SquadFSM.hs
+++ src/SC2/Army/SquadFSM.hs
@@ -1,60 +1,30 @@
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE StandaloneDeriving #-}
-
-{-# LANGUAGE ScopedTypeVariables #-}
-
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs, ExistentialQuantification, RankNTypes #-}
 
 module SC2.Army.SquadFSM where
 
 import SC2.Army.Behavior
-import Actions (Action (..), UnitTag)
-import Observation
 -- import SC2.Army.Army
 import SC2.Army.Utils
 import SC2.Army.Class
 import SC2.Army.Squad
-import SC2.Geometry
+    ( Squad(squadUnits, squadState), SquadFS(..), AnyFS(..), squadId )
 import SC2.Grid
-import SC2.Ids.AbilityId
-import SC2.Ids.UnitTypeId
-import SC2.Proto.Data (Alliance (..), Point, Point2D)
-import SC2.Proto.Data qualified as Proto
 import StepMonad
 import StepMonadUtils
-import Units
-import Utils
 
-import Conduit (filterC, mapC)
-import Control.Applicative ((<|>))
-import Control.Monad (filterM, void, when)
-import Data.Char (isDigit)
-import Data.Foldable qualified as Seq
-import Data.Function (on)
-import Data.HashMap.Strict (HashMap)
+import Control.Monad (void)
 import Data.HashMap.Strict qualified as HashMap
-import Data.HashSet qualified as HashSet
-import Data.List (minimumBy, partition)
-import Data.List.Split (chunksOf)
-import Data.Maybe (catMaybes, fromJust, isJust, isNothing, mapMaybe)
-import Data.Ord (comparing)
-import Data.Set (Set)
+import Data.Maybe (catMaybes)
 import Data.Set qualified as Set
 import Lens.Micro ((^.))
 import Lens.Micro.Extras (view)
-import Safe (headMay, minimumByMay)
-import System.Random (StdGen, randomR)
 
 import Debug.Trace (traceM)
 import Footprint
-import SC2.Proto.Data qualified as Proto
 
 import Data.Typeable
 
