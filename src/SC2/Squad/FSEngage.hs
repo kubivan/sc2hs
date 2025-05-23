52a28fce5e2826e706c79ba207588b2b375ebe32
kubivan <kubivan@users.noreply.github.com>
2025-05-23 17:38:11 +0300

    feat: implement simple hit&run behavior tree

--- src/SC2/Squad/FSEngage.hs
+++ src/SC2/Squad/FSEngage.hs
@@ -1,46 +1,102 @@
 
 module SC2.Squad.FSEngage where
 
-import SC2.Utils
 import Actions
 import Units
 import SC2.Grid
 import SC2.Squad.Class
 import SC2.Squad.Squad
-import SC2.Squad.Behavior
 import SC2.Geometry
 import StepMonad
 import StepMonadUtils
-import Actions (Action (..), UnitTag)
 import SC2.Ids.AbilityId
 import SC2.Ids.UnitTypeId
 import Observation
 
-import Control.Monad (void)
 import Data.HashMap.Strict qualified as HashMap
 import Data.Maybe
-import Data.Char (isDigit)
-import Data.ProtoLens (defMessage)
-import Data.Maybe (fromJust)
-import Conduit
 
-import Data.Set qualified as Set
 import Lens.Micro
 import Lens.Micro.Extras (view)
 
 import Debug.Trace
-import Footprint
 
-import Proto.S2clientprotocol.Common ( Point, Point2D )
-import Proto.S2clientprotocol.Common_Fields ( x, y, z )
-import Observation (getUnit)
-import SC2.Geometry (toPoint2D)
-import SC2.Squad.Class (HasArmy)
+import Proto.S2clientprotocol.Common ( Point2D )
 import Proto.S2clientprotocol.Data (UnitTypeData)
 
 data FSEngage = FSSeek UnitTag
 
---unitSeek :: HasArmy d => Unit -> Unit -> StepMonad d ()
+siUnitData :: AgentDynamicState d => Unit -> StepMonad d UnitTypeData
+siUnitData u = do
+    si <- agentStatic
+    let traits = unitTraits si
+        udata = (HashMap.!) traits . toEnum' . view #unitType $ u
+    return udata
+
+siUnitRange :: AgentDynamicState d => Unit -> Unit -> StepMonad d Float
+siUnitRange u e = do -- TODO: take into account different data.weapons: air/ground etc
+    udata <- siUnitData u
+    let range = view #range $ head $ udata ^. #weapons
+    return range
+
+isWeaponReady :: Unit -> Bool
+isWeaponReady u = (u ^. #weaponCooldown) == 0
+
+isEnemyInRange :: AgentDynamicState d => Unit -> Unit -> StepMonad d Bool
+isEnemyInRange e u = do
+    range <- siUnitRange u e
+    return $ distSquared (u ^. #pos) (e ^. #pos) <= range * range
+
+canAttack :: AgentDynamicState d => Unit -> Unit -> StepMonad d Bool
+canAttack u e = do
+    inRange <- isEnemyInRange e u
+    return $ isWeaponReady u && inRange
+
+stepForwardOrBack ::AgentDynamicState d =>  Unit -> Unit -> StepMonad d ()
+stepForwardOrBack u e = do
+    range <- siUnitRange u e
+    let distSq = distSquared (u ^. #pos) (e ^. #pos)
+        upos = toPoint2D $ u ^. #pos
+    if (distSq / 2 >= (range / 2) * (range / 2)) then do
+        stepForward <- u `stepToward` e
+        command [PointCommand Move [u] (upos + stepForward)] -- TODO: 1 stepForward vec
+    else do
+        stepBack <- stepBackward u e
+        command [PointCommand Move [u] (upos + stepBack) ]
+
+fleeVelocity :: Point2D -> Point2D -> Float -> Point2D
+fleeVelocity currentPos targetPos speed =
+  vecNormalize $ vecScale speed (currentPos - targetPos)
+
+seekVelocity :: Point2D -> Point2D -> Float -> Point2D
+seekVelocity currentPos targetPos speed =
+  vecNormalize $ vecScale speed (targetPos - currentPos)
+
+stepBackward :: AgentDynamicState d => Unit -> Unit -> StepMonad d Point2D
+stepBackward u e = do
+    udata <- siUnitData u
+    let uspeed = udata ^. #movementSpeed
+        enemyPos2D = toPoint2D $ e ^. #pos
+        unitPos2D = toPoint2D $ u ^. #pos
+    return $ fleeVelocity unitPos2D enemyPos2D uspeed
+
+stepToward :: AgentDynamicState d => Unit -> Unit -> StepMonad d Point2D
+stepToward u e = do
+    udata <- siUnitData u
+    let uspeed = udata ^. #movementSpeed
+        enemyPos2D = toPoint2D $ e ^. #pos
+        unitPos2D = toPoint2D $ u ^. #pos
+    return $ seekVelocity unitPos2D enemyPos2D uspeed
+
+unitEngageBehaviorTree :: AgentDynamicState d => Unit -> Unit -> StepMonad d ()
+unitEngageBehaviorTree u e = do
+    canAtk <- canAttack u e
+    if canAtk
+        then
+            command [UnitCommand AttackAttack [u] e]
+        else
+            stepForwardOrBack u e
+
 
 enemyStr :: Unit -> String
 enemyStr u = show (u ^. #tag) ++ ", " ++ show ((toEnum' (u ^. #unitType)) :: UnitTypeId) ++ " " ++ show (tilePos (u ^. #pos))
@@ -68,6 +124,8 @@ instance SquadFS FSEngage where
 
         debugUnit enemy
         mapM_ (\u -> debugUnitVec u (unitSeek u enemy)) units
+        mapM_ (`unitEngageBehaviorTree` enemy) units
+
 
     fsUpdate squad st@(FSSeek enemyTag) =
         do
@@ -83,6 +141,3 @@ instance SquadFS FSEngage where
 
     fsOnEnter s _ = traceM $ "[enter] FSEngage " ++ show (squadId s)
     fsOnExit  s _ = traceM $ "[exit] FSEngage " ++ show (squadId s)
-
--- data FSAttack = FSAttack Target
--- data FSEvade = FSEvade
