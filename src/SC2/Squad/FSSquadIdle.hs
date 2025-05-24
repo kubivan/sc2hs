95b75bd2d17c37277c806cc75cf936d401171592
kubivan <kubivan@users.noreply.github.com>
2025-05-24 23:28:00 +0300

    chore: clean FSSquadIdle

--- src/SC2/Squad/FSSquadIdle.hs
+++ src/SC2/Squad/FSSquadIdle.hs
@@ -1,34 +1,15 @@
 
 module SC2.Squad.FSSquadIdle where
 
-import SC2.Squad.Class
-import SC2.Utils
-import SC2.Army.Class
 import SC2.Squad.Squad
-import SC2.Grid
-import SC2.Geometry
-import StepMonad
-import StepMonadUtils
-import Actions (Action (..), UnitTag)
-import SC2.Ids.AbilityId
-
-import Control.Monad (void)
-import Data.HashMap.Strict qualified as HashMap
-import Data.Maybe
-import Data.Set qualified as Set
-import Lens.Micro ((^.))
-import Lens.Micro.Extras (view)
 
 import Debug.Trace (traceM)
-import Footprint
-
-import Data.Char (isDigit)
 
 data FSSquadIdle = FSSquadIdle
 
 instance SquadFS FSSquadIdle where
 
-    fsStep s _ = traceM "[step] idle" -- >> wanderAround 5
+    fsStep s _ = traceM "[step] idle" -- >> TODO: wanderAround 5
     fsUpdate _ st = pure (True, st)
     fsOnEnter s _ = traceM $ "[enter] Idle " ++ show (squadId s)
     fsOnExit  s _ = traceM $ "[exit] Idle " ++ show (squadId s)
